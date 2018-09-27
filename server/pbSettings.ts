f6a436f19f1a3628ddd56d2fe94a5a8d368a27c9
RinEddy <rineddy@gmail.com>
2018-09-27 22:32:17 +0200

    settings

--- server/pbSettings.ts
+++ server/pbSettings.ts
@@ -22,16 +22,16 @@ export namespace pbSettings {
 			maxNumberOfProblems: number;
 		};
 	}
-	/**
-	 * The global settings, used when the `workspace/configuration` request is not supported by the client.
-	 * Please note that this is not the case when using this server with the client provided in this example
-	 * but could happen with other clients.
-	 */
 	const DEFAULT_SETTINGS: ICustomizableSettings = {
 		diagnostics: {
 			maxNumberOfProblems: 1000
 		}
 	};
+	/**
+	 * The global settings, used when the `workspace/configuration` request is not supported by the client.
+	 * Please note that this is not the case when using this server with the client provided in this example
+	 * but could happen with other clients.
+	 */
 	let globalSettings: ICustomizableSettings = DEFAULT_SETTINGS;
 	/**
 	 * Cache the settings of all open documents
